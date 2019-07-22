ae0865b826dcdfe8fa72f565716ac7e4d7730d2b
hassan <alrashid334@gmail.com>
2019-07-22 08:18:06 +0300

    Auto complete components

--- src/app/material.ts
+++ src/app/material.ts
@@ -3,13 +3,31 @@ import { NgModule } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import {MatSelectModule} from '@angular/material/select';
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule,MatTableModule,MatToolbarModule,MatIconModule,MatExpansionModule,MatTabsModule,MatSelectModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatTableModule,MatToolbarModule,MatIconModule,MatExpansionModule,MatTabsModule,MatSelectModule],
+  imports: [MatButtonModule,
+     MatCheckboxModule,
+     MatTableModule,
+     MatToolbarModule,
+     MatIconModule,
+     MatExpansionModule,
+     MatTabsModule,
+     MatSelectModule,
+     MatAutocompleteModule
+    ],
+  exports: [MatButtonModule,
+     MatCheckboxModule,
+     MatTableModule,
+     MatToolbarModule,
+     MatIconModule,
+     MatExpansionModule,
+     MatTabsModule,
+     MatSelectModule,
+     MatAutocompleteModule
+    ],
 })
 export class MaterialModule { }
