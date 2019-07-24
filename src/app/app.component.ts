8099d324d4dc8e4ee1fda9be867d708157f91dca
shoruq <salufaisan@hotmail.com>
2019-07-24 11:20:35 +0300

    backend service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ELEMENT_DATA, PeriodicElement } from './models/PeriodicElement';
 
+import { BackendService } from './services/backend.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,10 +12,23 @@ export class AppComponent implements OnInit {
   title = 'Angular Project TEST';
   data: PeriodicElement[];
 
-  constructor(){}
+  constructor(private service: BackendService){}
 
   ngOnInit() {
-    this.data = ELEMENT_DATA; // subscribe to observable from the service
+    // this.data = ELEMENT_DATA; // subscribe to observable from the service //testing
+
+    this.service.getData(2).subscribe(
+      data => {
+        this.data = data;
+      },
+      /* data => {
+        console.log('backend working', data);
+      },*/ //testing
+
+      err => {
+        console.error(err);
+      }
+    )
   }
 
   getNextPage(pageNumber: number) {
