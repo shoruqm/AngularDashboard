56e88cd74c48ddc79381e262c6117ab1ce3c9474
hassan <alrashid334@gmail.com>
2019-07-29 14:32:49 +0300

    tracks navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,38 @@ import { BackendService } from './services/backend.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Angular Project TEST';
+  selectedTrack: string = '';
   data: PeriodicElement[];
+
+  tracks: {id: string, name: string}[] = [
+    {id: '0', name: 'Advanced Analytics'},
+    {id: '1', name: 'BlockChain'},
+    {id: '2', name: 'Mobility and AR/VR'},
+    {id: '3', name: 'Smart City'},
+    {id: '4', name: 'Smart Plant and IIoT'},
+  ]
 
   constructor(private service: BackendService){}
 
   ngOnInit() {
-    // this.data = ELEMENT_DATA; // subscribe to observable from the service //testing
 
     this.service.getData(1).subscribe(
       data => {
         this.data = data;
         console.log('got data', this.data);
       },
-      /* data => {
-        console.log('backend working', data);
-      },*/ //testing
-
       err => {
         console.error(err);
       }
     )
+  }
+
+  selectTrack(id: string) {
+    this.selectedTrack = id;
+  }
+
+  unselectTrack() {
+    this.selectedTrack = '';
   }
 
   getNextPage(pageNumber: number) {
